ddb996dd4228fdcf4e49dcb37a70a92c0c47795c
Dragan Nikolic <dragan.nikolic@inspirebrands.com>
2024-12-06 10:23:18 +0100

    update custom command line arguments example

--- 04_custom_cmdline/conftest.py
+++ 04_custom_cmdline/conftest.py
@@ -8,7 +8,19 @@ def pytest_addoption(parser):
     parser.addoption(
         "--date_column", action="store", help="date column for filtering data"
     )
+    parser.addoption(
+        "--no-docker", action="store_true", help="if true do not start docker"
+    )
 
+
+def pytest_sessionstart(session):
+    no_docker = session.config.getoption('--no-docker')
+    print(f'******* no-docker: {no_docker}')
+
+    if not no_docker:
+        print('*** run docker ***')
+    else:
+        print('*** dont run docker')
 
 @pytest.fixture
 def env(request):
